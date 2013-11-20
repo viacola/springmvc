66e7e56f1d7d7dd0b052f0d596902cc157a03666
bingo chen <viacola@gmail.com>
2013-11-20 18:24:48 +0800

    2

--- mvc/src/main/java/com/springapp/mvc/controller/UserRest.groovy
+++ mvc/src/main/java/com/springapp/mvc/controller/UserRest.groovy
@@ -1,5 +1,6 @@
 package com.springapp.mvc.controller
 
+import groovy.json.*
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,5 +9,44 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 class UserRest {
+	
+	
+	
+	@RequestMapping("/user")
+	public @ResponseBody String getUser() {
+		return """
+        {
+            "name":"bingo",
+            "age":34,
+            "address":"nanjing"
+        }
+        """;
+	}
+	
+	
+	@RequestMapping("/user2")
+	public @ResponseBody String getUser2() {
+		def user = new JsonBuilder();
+		def test ="AAAA"
+		user {
+			name 'bingo'
+			age  34
+			credit  test
+			address(
+					city: 'nanjing',
+					country: 'china',
+					zip: 210012
+				)  
+			married true
+			
+			departments 'TAP','Design dept.','Project Manager dept.'
+		}
+		
+		//user.pet name: 'dog'
+		
+		user.toPrettyString()
+	}
+	
+	
 	
 }
