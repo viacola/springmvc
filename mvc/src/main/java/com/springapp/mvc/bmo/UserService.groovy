796705523689c65aff58bbb280c44feae1a3f6a7
bing chen <viacola@gmail.com>
2013-11-23 14:45:07 +0800

    o

--- mvc/src/main/java/com/springapp/mvc/bmo/UserService.groovy
+++ mvc/src/main/java/com/springapp/mvc/bmo/UserService.groovy
@@ -1,15 +1,34 @@
 package com.springapp.mvc.bmo
 
+import groovy.sql.Sql
+import javax.sql.DataSource
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+
 class UserService {
+	@Autowired
+	DataSource dataSource
+	
 	public bingo(){
 		println("enter bingo()!")
 		"!bingo!"
 	}
 	
 	public insertUser(){
+		
+		def sql = new Sql(dataSource)
+		
+		sql.execute '''
+	     create table PROJECT (
+	         id integer not null,
+	         name varchar(50),
+	         url varchar(100),
+	     )
+	 '''
+		
+		
 		
 	}
 	
