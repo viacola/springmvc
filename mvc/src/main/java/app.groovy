af028b20f1aad72927a8d44f9b2201a289d3be05
bing chen <viacola@gmail.com>
2013-12-13 16:26:16 +0800

    提交spring boot mvc 架构的 REST服务

---
+++ mvc/src/main/java/app.groovy
@@ -0,0 +1,71 @@
+
+@Grab(group='com.gmongo', module='gmongo', version='1.0')
+import com.gmongo.GMongo
+
+
+@RestController
+class ThisWillActuallyRun {
+
+
+	@Autowired
+	private MyService myService;
+
+	def mongo = new GMongo()
+
+
+	def db = mongo.getDB("gmongo")
+
+    @RequestMapping("/")
+    String home(){
+
+       return "Hello World! 222\n"
+    }
+
+    @RequestMapping("/home")
+    String home2(){
+
+       return "Hello World! home\n"
+    } 
+
+    @RequestMapping("/insertDB/{name}")
+    String insertDB(@PathVariable("name") String theOwner){
+		db.languages.insert([name: theOwner])
+       return "insert languages ok !\n"
+    } 
+
+    @RequestMapping("/insertProduct/{name}")
+    String insertProduct(@PathVariable("name") String theName){
+		return myService.save(theName)
+    }
+    
+
+ 	@RequestMapping("/query/{name}")
+    String query(@PathVariable("name") String theName){
+		return db.orders.findOne()
+    }
+    
+
+}
+
+
+
+@Service
+class MyService {
+
+	def mongo = new GMongo()
+
+	def db = mongo.getDB("gmongo")
+
+	public String sayWorld() {
+		return "World!";
+	}
+
+	String save(String product) {
+		db.orders.insert(name: product)
+
+		return "insert $product ok !";
+	}
+
+
+}
+
